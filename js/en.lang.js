373af0c3977bdcdd314df5f8cf7c77e658b8eb63
Marcin Borkowicz <39220347+Masterboreq@users.noreply.github.com>
2018-10-15 16:41:47 +0200

    Add files via upload

---
+++ js/en.lang.js
@@ -0,0 +1,31 @@
+/* 
+	js/lang.js
+	Author: Marcin Borkowicz 2018
+	Package: Classic Tetris
+	Codebase: github.com/Masterboreq/tetris
+	Website: boreq.com/tetris
+	Comments: dictionary file for GUI language
+	Language: English
+*/
+
+var oLang = {
+	name: "English",
+	symbol: "en",
+	author: "Marcin Borkowicz"
+	},
+	
+	oGamePrompts = {
+		pause: "Pause",
+		gameOver: "Game over",
+		newGame: "New game",
+		beginGame: "Press Space bar to begin!",
+		endGame: "Do you want to end the game?"
+	},
+	
+	aEndGamePromts = [
+		"Bravo! Strong 2/10, hahaha!",
+		"C'mon! I haven't seen that bad game since long!",
+		"Is it your first time? No?! Oh, well...",
+		"Yes, yes - being master is not for everyone. Don't worry, it will never bother you!",
+		"Easy - your next game will be even worse, but keep on trying, hahaha!"
+	];
--- js/pl.lang.js
+++ js/pl.lang.js
@@ -5,15 +5,48 @@
 	Codebase: github.com/Masterboreq/tetris
 	Website: boreq.com/tetris
 	Comments: dictionary file for GUI language
+	Language: polski
 */
 
-var aGamePrompts = [
-		"Pauza",
-		"Koniec gry",
-		"Nowa gra",
-		"Naciśnij spację, aby rozpocząć grę!",
-		"Czy chcesz zakończyć grę?"
-	],
+var oLang = {
+	name: "Polski",
+	symbol: "pl",
+	author: "Marcin Borkowicz"
+	},
+	
+	oGameInterface = {
+		/* Elementy głównego interfejsu gry */
+		histogram: "",
+		nextPiece: "",
+		level: "",
+		score: "",
+		lines: "",
+		hiScore: "",
+		selectLevel: "",
+		
+		/* Przyciski i kontrolki */
+		startGameButton: "",
+		restartGameButton: "",
+		abortGameButton: "",
+		
+		/* Hiperłącza okna aplikacji */
+		aboutTetris: "O grze Tetris&trade;",
+		controls: "Sterowanie",
+		rules: "Zasady gry",
+		hiScoreTable: "Najlepsze wyniki",
+		settings: "Język &amp; temat",
+		repository: "Repozytorium",
+		translate: "Przetłumacz na swój język</a></li>",
+		aboutGame: "O aplikacji" //o aplikacji
+	},
+	
+	oGamePrompts = {
+		pause: "Pauza",
+		gameOver: "Koniec gry",
+		newGame: "Nowa gra",
+		beginGame: "Naciśnij spację, aby rozpocząć grę!",
+		endGame: "Czy chcesz zakończyć grę?"
+	},
 	
 	aEndGamePromts = [
 		"Brawo! Mocne 2/10, hahaha!",
